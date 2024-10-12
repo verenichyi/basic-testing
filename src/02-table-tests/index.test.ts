217fdbf2ade5556fe534069df63f1ee61de5617d
Yury_Viarenich <verenichyi@gmail.com>
2024-10-12 18:03:19 +0300

    chore: `02-table-test` remove redundant test cases

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -3,13 +3,11 @@ import { simpleCalculator, Action } from './index';
 
 const testCases = [
   { a: 1, b: 2, action: Action.Add, expected: 3 },
-  { a: 2, b: 2, action: Action.Add, expected: 4 },
-  { a: 3, b: 2, action: Action.Add, expected: 5 },
   { a: 5, b: 3, action: Action.Subtract, expected: 2 },
   { a: 6, b: 2, action: Action.Divide, expected: 3 },
   { a: 4, b: 3, action: Action.Multiply, expected: 12 },
   { a: 2, b: 3, action: Action.Exponentiate, expected: 8 },
-  { a: 10, b: 0, action: Action.Divide, expected: Infinity },
+  { a: 10, b: 11, action: 'Invalid action', expected: null },
   { a: '3423', b: [1], action: Action.Divide, expected: null },
 ];
 
